4bfcbb9ce4b7fcf11a9b4e0df152f9d62d92d5ca
Matt Mitchell <mmitche@microsoft.com>
2017-03-15 22:19:42 -0700

    Add abilty to set commit shas

---
+++ vars/setPRStatus.groovy
@@ -0,0 +1,42 @@
+// Sets a commit status for a PR.  Relies on the following:
+// pipeline must have been triggered by the GHPRB triggered
+// other parameters (auth, etc.) must be present in environment and valid
+// if they are not there, this step is skipped, commit status is echoed.
+// This file is a workaround to the inability seamlessly set this today.
+import org.kohsuke.github.GHCommitState;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+
+def call(String context, String state, String url, String subMessage = '') {
+    // Gather required parameters.  If missing, echo to let the
+    // owner know
+    def credentialsId = env["ghprbCredentialsId"]
+    if (credentialsId == "") {
+        echo "Could not find credentials ID (ghprbCredentialsId), ${context} (${subMessage}) is ${state}, see ${url}"
+        return
+    }
+    GhprbGitHubAuth auth = GhprbTrigger.getDscp().getGitHubAuth(credentialsId);
+
+    // Grab the project we're building
+    GitHub gh = auth.getConnection(currentBuild.rawBuild.getProject());
+
+    // Grab the repository associated
+    def repository = env["ghprbGhRepository"]
+    if (repository == "") {
+        echo "Could not find repository name (ghprbGhRepository), ${context} (${messubMessubMessagesagesage}) is ${state}, see ${url}"
+        return
+    }
+
+    GHRepository ghRepository = gh.getRepository(repository);
+
+    // Find the commit commitSha
+    def commitSha = env["ghprbActualCommit"]
+    if (commitSha == "") {
+        echo "Could not find sha (ghprbActualCommit), ${context} (${subMessage}) is ${state}, see ${url}"
+        return
+    }
+
+    // Create the state
+    ghRepository.createCommitStatus(commitSha, state, url, subMessage, context);
+}
