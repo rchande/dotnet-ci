66a617d5fc178fbecaa0f62a6160aeb8dc19ade6
Matt Mitchell <mmitche@microsoft.com>
2017-03-15 19:21:30 -0700

    Change standardDockerNode -> simpleDockerNode and import correct Utilities

--- vars/simpleDockerNode.groovy
+++ vars/simpleDockerNode.groovy
@@ -1,9 +1,9 @@
-import jobs.generation.Utilities
+import org.dotnet.ci.util.Agents
 
 // Runs a set of functionality on the default node
 // that supports docker.
 def call(String dockerImageName, Closure body) {
-    node (Utilities.getMachineAffinity('Ubuntu16.04', 'latest-or-auto-docker')) {
+    node (Agents.getMachineAffinity('Ubuntu16.04', 'latest-or-auto-docker')) {
         // Wrap in a try finally that cleans up the workspace
         try {
             // Wrap in the default timeout of 120 mins
