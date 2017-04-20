659820ac80f88ad2b7b64b775569df31d3ba5d19
Matt Mitchell <mmitche@microsoft.com>
2017-04-20 16:14:04 -0700

    Try something else

--- vars/simpleDockerNode.groovy
+++ vars/simpleDockerNode.groovy
@@ -32,12 +32,24 @@ def call(String dockerImageName, String hostVersion, Closure body) {
             retry (3) {
                 dockerImage.pull()
             }
+            // Important!! The current docker plugin always wants to pass -u hostuid:hostgid
+            // to the docker run step.  This is designed to work well with some of the other
+            // infrastructure, but causes havoc in our case because there are no non-default (root)
+            // users in our container.  This bug is filed as https://issues.jenkins-ci.org/browse/JENKINS-38438.
+            // In the meantime, we can pass -u to the inside() step which will append to the command line and
+            // override the original -u.
             dockerImage.inside('-u 0:0') {
                 try {
                     body()
                 }
                 finally {
-                    step([$class: 'WsCleanup'])
+                    // Normally we would use the workspace cleanup plugin to do this cleanup.
+                    // However in the latest versions, it utilizes the AsyncResourceDisposer.
+                    // This means that we won't attempt to delete the workspace until after the container
+                    // has exited.  Outside the container, we won't have permissions to delete the
+                    // mapped files (they will be root).
+                    echo "Cleaning workspace ${WORKSPACE}"
+                    dir(WORKSPACE) {deleteDir()}
                 }
             }
         }
