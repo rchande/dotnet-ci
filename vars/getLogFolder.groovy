c73e2ef563140d7aa8ab7035c1074a5d75965c23
Matt Mitchell <mmitche@microsoft.com>
2017-04-27 10:53:59 -0700

    Debugging

--- vars/getLogFolder.groovy
+++ vars/getLogFolder.groovy
@@ -6,10 +6,12 @@ def call() {
     echo "Automatically archived log folder will be at ${WORKSPACE}/netci-archived-logs/"
     String logFolder = "${WORKSPACE}/netci-archived-logs/"
     if (isUnix()) {
+        echo "if [ ! -d '${logFolder}' ]; then mkdir -p '${logFolder}'"
         sh "if [ ! -d '${logFolder}' ]; then mkdir -p '${logFolder}'"
     }
     else {
         bat "if NOT exists '${logFolder}' mkdir '${logFolder}'"
     }
+    echo logFolder
     return logFolder
 }
