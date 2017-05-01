e35f27a41d049bd50b4168de9fda0dca427d64c8
Matt Mitchell <mmitche@microsoft.com>
2017-05-01 09:45:35 -0700

    Correct format for results

--- vars/waitForHelixRuns.groovy
+++ vars/waitForHelixRuns.groovy
@@ -60,28 +60,57 @@ def call (def helixRunsBlob, String prStatusPrefix) {
                     def resultsResponse = httpRequest resultsUrl
                     def resultsContent = (new JsonSlurper()).parseText(resultsResponse.content)
 
-                    echo resultsContent
-
-                    // Some checks                    
+                    // Example content
+                    // If the data isn't complete, then Analysis will be empty.
+                    /*[
+                        {
+                            "Key": {
+                                "job.name": "0715528c-a31f-46ac-963e-8679c5880dc8"
+                            },
+                            "Data": {
+                                "Analysis": [
+                                    {
+                                        "Name": "xunit",
+                                        "Status": {
+                                            "pass": 374595,
+                                            "fail": 4,
+                                            "skip": 234
+                                        }
+                                    }
+                                ],
+                                "WorkItemStatus": {
+                                    "run": 1,
+                                    "pass": 204
+                                }
+                            }
+                        }
+                    ]*/
                     assert resultsContent.size() == 1 : "No results found for helix results API"
                     assert resultsContent[0].Data != null : "No data found in first result for helix results API"
-                    assert resultsContent[0].Data.Name == "xunit" : "Data in results api not xunit format"
-                    def passedTests = resultsContent[0].Data.Status.pass
-                    def failedTests = resultsContent[0].Data.Status.fail
-                    def skippedTests = resultsContent[0].Data.Status.skip
-                    def totalTests = passedTests + failedTests + skippedTests
-                    resultsContent = null
 
-                    def preStatus = isRunning ? "Running - " : ""
-                    // Compute the current resultValue.  We'll update the sub result every time, but the final result only when isFinished is true
-                    if (failedTests != 0) {
-                        resultValue = "FAILURE"
-                        subMessage = "${preStatus}Failed ${failedTests}/${totalTests} (${skippedTests} skipped)"
+                    if (resultsContent[0].Data.Analysis.size() == 0) {
+                        subMessage = "No results yet"
                     }
                     else {
-                        resultValue = "SUCCESS"
-                        subMessage = "${preStatus}Passed ${passedTests} (${skippedTests} skipped)"
+                        assert resultsContent[0].Data.Analysis.size() == 1 : "More than one set of analysis results"
+                        assert resultsContent[0].Data.Analysis[0].Name == "xunit" : "Data in results api not xunit format"
+                        def passedTests = resultsContent[0].Data.Analysis[0].Status.pass
+                        def failedTests = resultsContent[0].Data.Analysis[0].Status.fail
+                        def skippedTests = resultsContent[0].Data.Analysis[0].Status.skip
+                        def totalTests = passedTests + failedTests + skippedTests
+
+                        def preStatus = isRunning ? "Running - " : ""
+                        // Compute the current resultValue.  We'll update the sub result every time, but the final result only when isFinished is true
+                        if (failedTests != 0) {
+                            resultValue = "FAILURE"
+                            subMessage = "${preStatus}Failed ${failedTests}/${totalTests} (${skippedTests} skipped)"
+                        }
+                        else {
+                            resultValue = "SUCCESS"
+                            subMessage = "${preStatus}Passed ${passedTests} (${skippedTests} skipped)"
+                        }
                     }
+                    resultsContent = null
                 }
 
                 // We can also grab the info necessary to construct the link for Mission Control from this API.
